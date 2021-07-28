0342dd70aca4dc7e59875d14d1ba882c67ec991a
Ubuntu <ubuntu@ip-172-31-2-210.eu-west-2.compute.internal>
2021-07-28 15:40:03 +0000

    amend teststage script

--- jenkins/teststage.sh
+++ jenkins/teststage.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 source venv/bin/activate
-python3 -m pytest --cov=application 
---junitxml=junit/test-results.xml 
---cov-report=xml 
---cov-report=html
+python3 -m pytest --cov=application --junitxml=junit/test-results.xml --cov-report=xml --cov-report=html
